032ee7ab6ac94ad70af510a129ddd54ff16e1dc9
Алексей <alexeybudai@gmail.com>
2022-01-12 16:29:16 +0300

    complete HomeTask 9

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import HW7 from "../../h7/HW7";
 import HW8 from "../../h8/HW8";
+import HW9 from "../../h9/HW9";
 
 
 const Junior = () => {
     return (
-        <div style={{color:"white"}}>
-           <HW7/>
+        <div style={{color: "white"}}>
+            <HW7/>
             <HW8/>
+            <HW9/>
         </div>
     )
 }
